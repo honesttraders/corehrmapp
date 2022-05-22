0d7ad3b9e9037cc3a1aee28aa89812da9ca30cba
Foysal Dev <foysal.int@gmail.com>
2022-05-22 10:37:03 +0600

    updated

--- resources/lang/en/install.php
+++ resources/lang/en/install.php
@@ -1,12 +1,12 @@
 <?php
 return array(
     "welcome" => "Welcome",
-    "welcome_title" => "Welcome To Honest Traders",
+    "welcome_title" => "Welcome To Honest Traders Ltd",
 "admin_setup" => "Admin Setup",
 "done_msg" => "Installation done. You can now login.",
 "email" => "Email",
 "password" => "Password",
 "password_confirmation" => "Confirm Password",
 'with_demo_data' => 'Install With Demo Data',
-"welcome_description" => "Thank you for choosing InfixLms for online course management. Please follow the steps to complete Infix Lms installation!",
+"welcome_description" => "Thank you for choosing Hr24Hours for Human Resource Management. Please follow the steps to complete HRM installation!",
 );
--- src/Requests/UserRequest.php
+++ src/Requests/UserRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace HonestTraders\CoreService\Requests;
+namespace HonestTraders\CoreHrmApp\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
